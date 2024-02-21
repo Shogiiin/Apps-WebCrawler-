a963105b67d366897bcff479c78db35a717544c8
Shogiiin <142786566+Shogiiin@users.noreply.github.com>
2024-02-21 19:44:37 +0100

    Added write output to file and cloudflare detection

--- src/index.js
+++ src/index.js
@@ -20,12 +20,25 @@ let maxPageNum = 1;
 (async () => {
 
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({ headless: false })
+    const browser = await puppeteer.launch({ headless: true })
     const page = await browser.newPage()
 
-    // accept cookies :)
     await page.goto('https://stackoverflow.com')
-    await page.waitForSelector('#onetrust-accept-btn-handler');
+
+    //check if cloudflare is active
+    await page.waitForSelector('body')
+    if(await page.evaluate(() => {
+        const documentBody = document.querySelector('body')
+        // if(documentBody.className == "no-js") return true
+        return documentBody.classList.contains("no-js")
+    })) {
+        console.error('Cloudflare kicked in.')
+        await browser.close()
+        return
+    }
+
+    // accept cookies :)
+    await page.waitForSelector('#onetrust-accept-btn-handler')
     await page.click('#onetrust-accept-btn-handler')
     // await page.waitForNavigation({waitUntil: 'networkidle2'})
     // await page.click('#onetrust-accept-btn-handler')
@@ -42,12 +55,37 @@ let maxPageNum = 1;
 
 
         await page.goto(`https://stackoverflow.com/questions/tagged/${tag}?tab=newest&page=1&pagesize=50`)
+
+        //check if cloudflare is active
+        await page.waitForSelector('body')
+        if(await page.evaluate(() => {
+            const documentBody = document.querySelector('body')
+            // if(documentBody.className == "no-js") return true
+            return documentBody.classList.contains("no-js")
+        })) {
+            console.error('Cloudflare kicked in.')
+            await browser.close()
+            return
+        }
+
         maxPageNum = await page.evaluate((pageNum) => {
             const pageButtons = document.querySelectorAll('.s-pagination--item')
             return Number(pageButtons[10].textContent)
         })
         while(pageNum <= maxPageNum) {
             await page.goto(`https://stackoverflow.com/questions/tagged/${tag}?tab=newest&page=${pageNum}&pagesize=50`)
+
+            //check if cloudflare is active
+            await page.waitForSelector('body')
+            if(await page.evaluate(() => {
+                const documentBody = document.querySelector('body')
+                // if(documentBody.className == "no-js") return true
+                return documentBody.classList.contains("no-js")
+            })) {
+                console.error('Cloudflare kicked in.')
+                await browser.close()
+                return
+            }
             
             const userLinks = await page.evaluate(() => {
                 const userElements = document.querySelectorAll('.s-avatar')
@@ -61,6 +99,18 @@ let maxPageNum = 1;
             let count = 0
             for(const userLink of userLinks) {
                 await page.goto(`https://stackoverflow.com/${userLink}`)
+
+                //check if cloudflare is active
+                await page.waitForSelector('body')
+                if(await page.evaluate(() => {
+                    const documentBody = document.querySelector('body')
+                    // if(documentBody.className == "no-js") return true
+                    return documentBody.classList.contains("no-js")
+                })) {
+                    console.error('Cloudflare kicked in.')
+                    await browser.close()
+                    return
+                }
 
                 const username = await page.evaluate(() => {
                     if(document.querySelector('.flex--item.mb12.fs-headline2.lh-xs')) 
@@ -78,6 +128,22 @@ let maxPageNum = 1;
                     return null
                 })
 
+                
+                //check if user is saved
+                if(fs.existsSync(`Output/users-${tag}`)) {
+                    let savedUsers = []
+                    savedUsers = JSON.parse(fs.readFileSync(`Output/users-${tag}`))
+                    if(savedUsers.filter(e => e.username === username).length > 0) {
+                        // console.log(`Skipped adding user : ${username}`)
+                        continue
+                    }
+                }
+
+                //checks if user is from germany
+                // if(!ort === null) {
+                //     if(!ort.includes('Germany')) continue
+                // }
+                
                 if(users.filter(e => e.username === username).length > 0) continue
                 users.push({ username: username, ort: ort, job: job })
                 // console.log(count)
@@ -86,12 +152,56 @@ let maxPageNum = 1;
             }
             if(count > 5) break;
         }
-        console.log(`Tag : "${tag}" :`)
+        
+        const path = `Output/users-${tag}`
+
+        //write info to designated file if there is any
+
+        if (!fs.existsSync('Output')) {
+            fs.mkdirSync('Output', { recursive: true });
+        }
+
         console.log(users)
-        await timeout(5000)
+        if(users.length == 0) {
+            console.log(`No (new) users found for ${tag}, continuing with next tag.`)
+            continue
+        }
+
+        if(fs.existsSync(path)) {
+            const fileContent = fs.readFileSync(path)
+            let JSONdataToWrite = []
+            JSONdataToWrite = JSON.parse(fileContent)
+
+            for(const user of users) {
+                JSONdataToWrite.push(user)
+            }
+
+            const dataToWrite = JSON.stringify(JSONdataToWrite)
+
+            fs.writeFile(path, dataToWrite, (err) => {
+                if(err) throw err
+                console.log(`File edited! : ${path}`)
+            })
+        } else {
+            let counter = 0
+            let dataToWrite = '['
+            for(const user of users) {
+                if(counter>0) dataToWrite += ','
+                dataToWrite += JSON.stringify(user)
+                counter++
+            }
+            dataToWrite += ']'
+
+            fs.writeFile(path, dataToWrite, (err) => {
+                if(err) throw err
+                console.log(`File created! : ${path}`)
+            })
+        }
+
+        // await timeout(5000)
     }
 
     // close Browser after 5 sec
-    timeout(3000)
+    // timeout(3000)
     await browser.close()
 })()
