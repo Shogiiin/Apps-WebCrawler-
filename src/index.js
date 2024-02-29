83bd032d554c4c5bca642cf796f3b0e459340bff
Shogiiin <lukaskonsorski@gmail.com>
2024-02-29 14:18:00 +0100

    Added part of date comparison

--- src/index.js
+++ src/index.js
@@ -50,6 +50,8 @@ function writeToCSV(filePath, data) {
         let pageNum = 1;
         let maxPageNum = 1;
 
+        let dateBrake = 1
+
         await page.goto(`https://stackoverflow.com/questions/tagged/${tag}?tab=newest&page=${pageNum}&pagesize=50`);
 
         maxPageNum = await page.evaluate(() => {
@@ -57,7 +59,7 @@ function writeToCSV(filePath, data) {
             return Number(pageButtons[10].textContent);
         });
 
-        while (pageNum <= maxPageNum && users.length < 4) {
+        while (pageNum <= maxPageNum && users.length < 4 && dateBrake > 0) {
             await page.goto(`https://stackoverflow.com/questions/tagged/${tag}?tab=newest&page=${pageNum}&pagesize=50`);
 
             const userLinks = await page.evaluate(() => {
@@ -65,38 +67,73 @@ function writeToCSV(filePath, data) {
                 const links = [];
                 for (const ele of userElements) {
                     if (String(ele.getAttribute('href')).includes('users'))
-                        links.push(ele.getAttribute('href'));
-                }
+                    links.push(ele.getAttribute('href'));
+            }
                 return links;
             });
+
+            let lastDate = new Date()
+        
+            lastDateArray = await page.evaluate(() => {
+                const timeElements = document.querySelectorAll('time.s-user-card--time');
+                const date = new Date()
+                let dateArray
+                
+                ele = timeElements[0]
+                if(ele.innerHTML.includes('hour') || ele.innerHTML.includes('day')) {
+                    dateArray = Date()
+                } else {
+                    
+                }
+
+                // dateArray = [date.getDate(), Number(date.getMonth()+1), date.getFullYear()]
+                return dateArray;
+            });
+
+            console.log(lastDateArray)
+            
+            lastDate.setDate(lastDateArray[0])
+            lastDate.setMonth(lastDateArray[1])
+            lastDate.setYear(lastDateArray[2])
+            
+            console.log(lastDate)
+            lastDate.setYear(1969)
+
+            const todayDate = new Date()
+            if(todayDate > lastDate) {
+                dateBrake = -1;
+                console.log("today>last")
+                break;
+            }
+
 
             for (const userLink of userLinks) {
                 if (users.length >= 4) break;
 
-                await page.goto(`https://stackoverflow.com/${userLink}`);
+                // await page.goto(`https://stackoverflow.com/${userLink}`);
 
-                const username = await page.evaluate(() => {
-                    const usernameElement = document.querySelector('.flex--item.mb12.fs-headline2.lh-xs');
-                    return usernameElement ? usernameElement.textContent.trim() : null;
-                });
+                // const username = await page.evaluate(() => {
+                //     const usernameElement = document.querySelector('.flex--item.mb12.fs-headline2.lh-xs');
+                //     return usernameElement ? usernameElement.textContent.trim() : null;
+                // });
 
-                const job = await page.evaluate(() => {
-                    const jobElement = document.querySelector('.mb8.fc-black-400.fs-title.lh-xs');
-                    return jobElement ? jobElement.textContent.trim() : 'NULL';
-                });
+                // const job = await page.evaluate(() => {
+                //     const jobElement = document.querySelector('.mb8.fc-black-400.fs-title.lh-xs');
+                //     return jobElement ? jobElement.textContent.trim() : 'NULL';
+                // });
 
-                const ort = await page.evaluate(() => {
-                    const ortElement = document.querySelector('.wmx2.truncate');
-                    return ortElement ? ortElement.textContent.trim() : 'NULL';
-                });
-                if(ort.toLowerCase().includes('germany')) continue;
+                // const ort = await page.evaluate(() => {
+                //     const ortElement = document.querySelector('.wmx2.truncate');
+                //     return ortElement ? ortElement.textContent.trim() : 'NULL';
+                // });
+                // if(ort.toLowerCase().includes('germany')) continue;
 
-                // Überprüfen, ob Benutzer bereits existiert
-                const userExists = users.find(user => user.username === username && user.ort === ort && user.job === job);
-                if (!userExists) {
-                    // Benutzerdaten zum Array hinzufügen
-                    users.push({ username: username || 'NULL', ort: ort || 'NULL', job: job || 'NULL' });
-                }
+                // // Überprüfen, ob Benutzer bereits existiert
+                // const userExists = users.find(user => user.username === username && user.ort === ort && user.job === job);
+                // if (!userExists) {
+                //     // Benutzerdaten zum Array hinzufügen
+                //     users.push({ username: username || 'NULL', ort: ort || 'NULL', job: job || 'NULL' });
+                // }
             }
 
             pageNum++;
